9fea58db1960529ca8c6ee4ef041a7a0da6f4590
yasindemirkaya <yasin97demirkaya@gmail.com>
2025-04-27 00:58:09 +0300

    main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://hyper4:hyper4123@webprojectscluster.rdtsnfh.mongodb.net/NestRestAPI?retryWrites=true&w=majority&appName=WebProjectsCluster')
+  ],
 })
-export class AppModule {}
+export class AppModule { }
